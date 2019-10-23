54fb290f7b1a081d5ca79191e72fe065ec07608e
RizkyGilang <rizkygilang839@gmail.com>
2019-10-24 05:28:53 +0700

    welcome activity

--- app/src/main/java/ac/id/uts/Activities/WelcomeActivity.java
+++ app/src/main/java/ac/id/uts/Activities/WelcomeActivity.java
@@ -2,7 +2,9 @@ package ac.id.uts.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import ac.id.uts.R;
 
@@ -12,5 +14,12 @@ public class WelcomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(WelcomeActivity.this,MainActivity.class));
+                finish();
+            }
+        },5000);
     }
 }
